f5bf5816179a4ce961e1a858aec1186da125352b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-09-04 14:01:35 +0200

    init - add generate.sh file

---
+++ generate.sh
@@ -0,0 +1,35 @@
+# get the latest wsdltophp.phar
+if [ ! -f wsdltophp.phar ]; then
+    echo "Download wsdltophp.phar once";
+    wget https://phar.wsdltophp.com/wsdltophp.phar;
+fi
+
+# get current folder
+DEST=`pwd`;
+
+# clean current folder
+rm -rf  $DEST/src/ \
+        $DEST/tutorial.php \
+        $DEST/composer.json \
+        $DEST/composer.lock;
+
+# package informations
+php wsdltophp.phar generate:package \
+    --urlorpath=$DEST"/wsdl/services.wsdl" \
+    --destination=$DEST \
+    --composer-name="wsdltophp/package-ews" \
+    --addcomments="author:WsdlToPhp <contact@wsdltophp.com>" \
+    --addcomments="release:1.0.0" \
+    --soapclient="\SoapClient\SoapClientBase" \
+    --prefix="Ews";
+
+# generate package
+php wsdltophp.phar generate:package \
+    --urlorpath=$DEST"/wsdl/services.wsdl" \
+    --destination=$DEST \
+    --composer-name="wsdltophp/package-ews" \
+    --addcomments="author:WsdlToPhp <contact@wsdltophp.com>" \
+    --addcomments="release:1.0.0" \
+    --soapclient="\SoapClient\SoapClientBase" \
+    --prefix="Ews" \
+    --force;
